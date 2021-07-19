fd6b4e05289ccc1d759215214925f1071e587b16
iPriest001 <1215430211@qq.com>
2021-07-19 13:57:55 +0800

    add backbone "gvt" Twins

--- configs/retinanet/retinanet_r50_fpn_1x_dior.py
+++ configs/retinanet/retinanet_r50_fpn_1x_dior.py
@@ -4,11 +4,22 @@ _base_ = [
 ]
 model = dict(bbox_head=dict(num_classes=20))
 # optimizer
+"""
 optimizer = dict(type='SGD', lr=0.00125, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
 # actual epoch = 3 * 3 = 9
 lr_config = dict(policy='step', step=[3])
+"""
+optimizer = dict(type='AdamW', lr=0.0001, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=500,
+    warmup_ratio=0.001,
+    step=[8, 11])
 # runtime settings
 runner = dict(
-    type='EpochBasedRunner', max_epochs=4)  # actual epoch = 4 * 3 = 12
+    type='EpochBasedRunner', max_epochs=12)  # actual epoch = 4 * 3 = 12
--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -11,10 +11,11 @@ from .resnext import ResNeXt
 from .ssd_vgg import SSDVGG
 from .trident_resnet import TridentResNet
 from .gvt import alt_gvt_small, alt_gvt_base, alt_gvt_large, pcpvt_small, pcpvt_base, pcpvt_large
+from .swin_transformer import SwinTransformer
 
 __all__ = [
     'RegNet', 'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'Res2Net',
     'HourglassNet', 'DetectoRS_ResNet', 'DetectoRS_ResNeXt', 'Darknet',
     'ResNeSt', 'TridentResNet', 'alt_gvt_small', 'alt_gvt_base', 'alt_gvt_large', 'pcpvt_small',
-    'pcpvt_base', 'pcpvt_large'
+    'pcpvt_base', 'pcpvt_large', 'SwinTransformer'
 ]
