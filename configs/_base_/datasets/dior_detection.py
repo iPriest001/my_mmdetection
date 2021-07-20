2719232f401532e84d03747107c73a7954d9873c
iPriest001 <1215430211@qq.com>
2021-07-20 21:29:17 +0800

    change dior dataset train&test config : SGD, image short side=800

--- configs/_base_/datasets/dior_detection.py
+++ configs/_base_/datasets/dior_detection.py
@@ -6,7 +6,7 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1000, 600), keep_ratio=True),
+    dict(type='Resize', img_scale=(1000, 800), keep_ratio=True), # short side = 800 for dior dataset
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -17,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(1000, 600),
+        img_scale=(1000, 800), # short side = 800 for dior dataset
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -37,13 +37,13 @@ data = dict(
         dataset=dict(
             type=dataset_type,
             ann_file=[
-                data_root + 'ImageSets/Main/train.txt'
+                data_root + 'ImageSets/Main/trainval.txt'
             ],
             img_prefix=data_root,
             pipeline=train_pipeline)),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'ImageSets/Main/val.txt',
+        ann_file=data_root + 'ImageSets/Main/test.txt',
         img_prefix=data_root,
         pipeline=test_pipeline),
     test=dict(
--- configs/retinanet/retinanet_r50_fpn_1x_dior.py
+++ configs/retinanet/retinanet_r50_fpn_1x_dior.py
@@ -4,7 +4,6 @@ _base_ = [
 ]
 model = dict(bbox_head=dict(num_classes=20))
 # optimizer
-"""
 optimizer = dict(type='SGD', lr=0.00125, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=None)
 # learning policy
@@ -20,6 +19,7 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=0.001,
     step=[8, 11])
+"""
 # runtime settings
 runner = dict(
-    type='EpochBasedRunner', max_epochs=12)  # actual epoch = 4 * 3 = 12
+    type='EpochBasedRunner', max_epochs=4)  # actual epoch = 4 * 3 = 12
